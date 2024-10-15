9ef14985c42e625806515d9e5285d0ca414a2802
Priyanshu Chouhan <priyanschouhan123@gmail.com>
2024-10-16 00:19:13 +0530

    "Adding Shadcn and Aceternity UI"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,28 @@
+import { BackgroundBeamsWithCollision } from "@/components/ui/background-beams-with-collision";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-screen p-8 pb-20 gap-16 sm:p-20">
-      <div className="">
-        <h1 className="text-3xl font-bold text-center mb-5">
-          Welcome to Smart Classroom
-        </h1>
-        <p className="text-center">We&apos;re coming soon...</p>
-      </div>
-      <div className="fixed bottom-10 p-2 text-center">
-        Designed and Developed by <Link className="text-amber-400" href={'https://github.com/sureshchoudharysirvi'} target="_blank">Suresh Choudhary</Link> & <Link className="text-amber-400" href={'https://github.com/priynshuchouhn'} target="_blank">Priyanshu Chouhan</Link> 
-      </div>
+    <div className="flex items-center justify-center h-screen pb-20 sm:p-20">
+      <BackgroundBeamsWithCollision>
+        <div>
+          <h2 className="text-2xl relative z-20 md:text-4xl lg:text-7xl font-bold text-center text-black font-sans tracking-tight">
+            welcome to{" "}
+            <div className="relative mx-auto inline-block w-max [filter:drop-shadow(0px_1px_3px_rgba(27,_37,_80,_0.14))]">
+              <div className="absolute left-0 top-[1px] bg-clip-text bg-no-repeat text-transparent bg-gradient-to-r py-4 from-purple-500 via-violet-500 to-pink-500 [text-shadow:0_0_rgba(0,0,0,0.1)]">
+                <span className="">Smart Classroom</span>
+              </div>
+              <div className="relative bg-clip-text text-transparent bg-no-repeat bg-gradient-to-r from-purple-500 via-violet-500 to-pink-500 py-4">
+                <span className="">Smart Classroom</span>
+              </div>
+            </div>
+          </h2>
+          <p className="text-center">We&apos;re coming soon...</p>
+        </div>
+        <div className="fixed bottom-5 p-2 text-center">
+          Designed and Developed by <Link className="text-purple-800" href={'https://github.com/sureshchoudharysirvi'} target="_blank">Suresh Choudhary</Link> & <Link className="text-purple-800" href={'https://github.com/priynshuchouhn'} target="_blank">Priyanshu Chouhan</Link>
+        </div>
+      </BackgroundBeamsWithCollision>
     </div>
   );
 }
