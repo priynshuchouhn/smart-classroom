502a69ac89ee2c92e81793bb21de83f303a54cc6
Priyanshu Chouhan <priyanschouhan123@gmail.com>
2024-11-03 19:23:27 +0530

    "Added new dependencies to package.json, updated snapshots, and modified CoursePage component to include a Button and Link from next/link"

--- src/app/(platform)/instructor/courses/page.tsx
+++ src/app/(platform)/instructor/courses/page.tsx
@@ -1,9 +1,15 @@
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 import React from 'react'
 
 function CoursePage() {
   return (
-    <div>
-      
+    <div className='p-6'>
+      <Link href={'/instructor/courses/create'}>
+        <Button>
+          New Course
+        </Button>
+      </Link>
     </div>
   )
 }
