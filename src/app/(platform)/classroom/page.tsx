cd118a2e6fcebde341ae8aebbcfa173a3994b961
Priyanshu Chouhan <priynshuchouhn@gmail.com>
2025-01-16 12:30:46 +0530

    `Refactor: Remove unused imports and code, simplify Page function`

--- src/app/(platform)/classroom/page.tsx
+++ src/app/(platform)/classroom/page.tsx
@@ -1,23 +1,12 @@
-'use client'
 import { getDashboardModules } from '@/actions/get-courses';
 import { ModulesList } from '@/components/ui/modules-list';
-import { ModuleWithProgressWithSubject } from '@/types';
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
-function Page() {
+async function Page() {
   const { userId } = { userId: "Hello world" };
-  const [modules, setModules] = useState<ModuleWithProgressWithSubject[]>([]);
-  useEffect(()=>{
-    async function fetchModules(){
-      const modulesWithSubject = await getDashboardModules({
-        userId,
-      });
-      console.log("[Modules Fetched]", modulesWithSubject);
-      setModules(modulesWithSubject)
-    }
-    fetchModules();
-  },[userId])
-  console.log(modules)
+  const modules = await getDashboardModules({
+    userId,
+  });
   return (
     <>
       <div className="p-6 space-y-4">
