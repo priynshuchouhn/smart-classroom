b45758fd3f9326c56721e0e6c7650af08fa07e13
Priyanshu Chouhan <priynshuchouhn@gmail.com>
2025-01-16 12:28:11 +0530

    `feat: updated page.tsx with use client, added module type import, and refactored async function to use useEffect and useState`

--- src/app/(platform)/classroom/page.tsx
+++ src/app/(platform)/classroom/page.tsx
@@ -1,12 +1,22 @@
+'use client'
 import { getDashboardModules } from '@/actions/get-courses';
 import { ModulesList } from '@/components/ui/modules-list';
-import React from 'react'
+import { ModuleWithProgressWithSubject } from '@/types';
+import React, { useEffect, useState } from 'react'
 
-async function Page() {
+function Page() {
   const { userId } = { userId: "Hello world" };
-  const modules = await getDashboardModules({
-    userId,
-  });
+  const [modules, setModules] = useState<ModuleWithProgressWithSubject[]>([]);
+  useEffect(()=>{
+    async function fetchModules(){
+      const modulesWithSubject = await getDashboardModules({
+        userId,
+      });
+      console.log("[Modules Fetched]", modulesWithSubject);
+      setModules(modulesWithSubject)
+    }
+    fetchModules();
+  },[userId])
   console.log(modules)
   return (
     <>
